c73ceb13471490e61e6f69cfd7bb16b79d7a9893
Shosan Vader <okediadip@gmail.com>
2023-09-08 18:01:55 +0100

    Added urls.py and edited views

--- api/views.py
+++ api/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+import datetime
 
-# Create your views here.
+from django.http import JsonResponse
+from django.shortcuts import render
+from django.utils import timezone
+from django.views.decorators.http import require_GET
+
+Github_File_Url = 'https://github.com/Shosanvader/hng-projects/blob/main/slackproject/manage.py'
+Github_Repo_Url = 'https://github.com/Shosanvader/hng-projects'
+
+@require_GET
+def get_info(request):
+    slack_name = request.GET.get('slack_name')
+    track = request.GET.get('track')
+
+    current_day = datetime.datetime.now().strftime('%A')
+    current_utc_time = timezone.now().strftime('%Y-%m-%dT%H:%M:%SZ')
+
+    response = {
+        'slack_name': slack_name,
+        'current_day': current_day,
+        'utc_time': current_utc_time,
+        'track': track,
+        'github_file_url': Github_File_Url,
+        'github_repo_url': Github_Repo_Url,
+        'status_code': 200
+    }
+
+    return JsonResponse(response)
+
